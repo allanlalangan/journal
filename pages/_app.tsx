fd283fd1b03d8775d8bf3eca813ead34cd65fbaf
allanlalangan <allanlalangan@gmail.com>
2023-02-16 22:01:57 -0800

    feat: insert Header and Sidebar with grid layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className='grid grid-cols-12'>
+			<Header />
+			<Sidebar />
+			<Component {...pageProps} />
+		</div>
+	);
 }
